ba7bc2c12c4e89911215c314bd83479e539ec1a0
Giovanni <73745067+PiNePu447@users.noreply.github.com>
2024-11-26 19:04:51 -0300

    Features/class dashboard (#6)
    * classes page
    
    * integrate class page with store
    
    * added email func
    
    * pr fixes

--- design-thinking-ed-platform-be/src/modules/class/class.controller.ts
+++ design-thinking-ed-platform-be/src/modules/class/class.controller.ts
@@ -11,15 +11,17 @@ import { ClassService } from './class.service';
 import { CreateClassDto } from './dto/create-class.dto';
 import { UpdateClassDto } from './dto/update-class.dto';
 import { ProjectSteps } from 'src/common/enum/project.enum';
+import { UserService } from '../user/user.service';
 
 @Controller('class')
 export class ClassController {
-  constructor(private readonly classService: ClassService) {}
+  constructor(private readonly classService: ClassService, private readonly userService: UserService) { }
 
   @Post()
   create(@Body() createClassDto: CreateClassDto) {
     createClassDto.projectStep = ProjectSteps.EMPATHY;
     createClassDto.invitedStudents = [createClassDto.invitedStudents].flat();
+    this.userService.inviteUsers(createClassDto.invitedStudents)
     return this.classService.create(createClassDto);
   }
 
--- design-thinking-ed-platform-be/src/modules/class/class.module.ts
+++ design-thinking-ed-platform-be/src/modules/class/class.module.ts
@@ -3,10 +3,12 @@ import { ClassService } from './class.service';
 import { ClassController } from './class.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClassEntity } from './entities/class.entity';
+import { UserModule } from '../user/user.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClassEntity])],
+  imports: [TypeOrmModule.forFeature([ClassEntity]), UserModule],
   controllers: [ClassController],
   providers: [ClassService],
 })
-export class ClassModule {}
+export class ClassModule { }
